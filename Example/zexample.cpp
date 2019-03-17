fa5a3cc2d1231047120a803dd099d33ee796decc
Tianli ZHOU <zhoutl1106@hotmail.com>
2019-03-17 12:35:04 -0500

    update example

--- Example/zexample.cpp
+++ Example/zexample.cpp
@@ -30,14 +30,15 @@ void ZExample::single(int argc, char *argv[])
     if(true)//argc == 4)
     {
         int K,M,W,costf,stra;
-        K = 5,M=2,W=4;
+        cin >> K >> M >> W;
+        costf = atoi(argv[2]);
+        stra = atoi(argv[3]);
         vector<int> arr(K+M,0);
-//        cin >> K >> M >> W;
-//        for(int i = 0;i<K+M;i++)
-//            cin >> arr[i];
-        arr[0] = 6,arr[1] = 1,arr[2] = 7,arr[3] = 5,arr[4] = 9,arr[5] = 3,arr[6] = 11;
-        costf = 2;//atoi(argv[2]);
-        stra = 7;//atoi(argv[3]);
+        printf("%d\n",arr.size());
+        for(int i = 0;i<K+M;i++)
+            cin >> arr[i];
+//        arr[0] = 6,arr[1] = 1,arr[2] = 7,arr[3] = 5,arr[4] = 9,arr[5] = 3,arr[6] = 11;
+
         printf("cost_func=%d,strategy=%d,K=%d,M=%d,W=%d,arr=",costf,stra,K,M,W);
         for(int i = 0;i<K+M;i++)
             printf("%d ",arr[i]);
